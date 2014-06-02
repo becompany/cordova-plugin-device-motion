7f2a613937a895c809dddc04a5b752182f8322f3
Roman Fuerst <roman.fuerst@becompany.ch>
2014-06-02 16:42:22 +0200

    Update plugin.xml. Update package name.

--- src/android/AccelListener.java
+++ src/android/AccelListener.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.devicemotion;
+package ch.becompany.cordova.devicemotion;
 
 import java.util.List;
 
